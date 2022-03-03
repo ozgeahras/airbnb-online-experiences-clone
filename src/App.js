bf5c58517e2fad7b654182efefd6f7b05fcfbda5
Özge Ahras <ozgeahras@gmail.com>
2022-03-03 21:25:32 +0300

    update for app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar.js";
 import Hero from "./components/Hero.js";
-import Section from "./components/Section.js";
 import Card from "./components/Card.js";
 import "./App.css";
 import data from "./data.js";
@@ -15,7 +14,6 @@ export default function App() {
     <div>
       <Navbar />
       <Hero />
-      <Section />
       <section className="cards-list">{card}</section>
     </div>
   );
