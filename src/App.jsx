4326c746bace854385b93cbb44512bb4506b1de5
ozgeahras <ozgeahras@gmail.com>
2023-04-20 15:06:27 +0300

    fixed pages images problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Card from "./components/Card";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Card from "./components/Card.jsx";
 import data from "../data.js";
 import "./App.css";
 
