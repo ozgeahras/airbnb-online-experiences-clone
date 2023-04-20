e3cfcc16917d8ca0e24c50ccc1e238f5a16d45fa
ozgeahras <ozgeahras@gmail.com>
2023-04-20 15:39:16 +0300

    fixed images in card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,7 +11,10 @@ export default function Card(props) {
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={`./src/images/${props.coverImg}`} className="card--image" />
+      <img
+        src={`./airbnb-online-experiences-clone/src/images/${props.coverImg}`}
+        className="card--image"
+      />
       <div className="card--stats">
         <img src="./src/images/star.png" className="card--star" />
         <span>{props.stats.rating}</span>
